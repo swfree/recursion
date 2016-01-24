783d8cc50a781dcec06a69bd321244a621e76e43
swfree <skye.free@gmail.com>
2016-01-24 13:03:48 -0500

    Write stringifyJSON recursive function

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,39 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  //base cases
+  if (typeof obj !== 'object'){
+    if (typeof obj === 'string'){
+      return '"' + obj + '"';
+    }
+    return "" + obj;
+  }
+  if (obj === null){
+    return "" + obj;
+  }
+
+  //recursive case
+  var result = "";
+  for (var prop in obj){
+    //skips over key/val pairs with undefined or functions
+    if(typeof obj[prop] === "undefined" || typeof obj[prop] === "function"){ continue; }
+
+    if(result !== ""){
+      result += ","; 
+    }
+    
+    if (Array.isArray(obj)) {
+      result += stringifyJSON(obj[prop]);
+    } else {
+      result += '"' + prop + '"' + ":" + stringifyJSON(obj[prop]);    
+    }
+  }
+
+  if (Array.isArray(obj)){
+    return '[' + result + ']'; 
+  } else {
+    return '{' + result + '}';
+  }
+  return result;
+
 };
